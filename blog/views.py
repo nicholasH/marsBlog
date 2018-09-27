8b0d10c3204bd81b8da497aca35094916511f65f
Nicholas Hoover <n.hoover13@gmail.com>
2018-09-27 16:32:19 -0700

    work

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from .models import Blog
+from .models import Blog , Gallery
 
 def blog(request):
     blog = Blog.objects.all()
+
+
+
     return render(request, 'blog/blog.html', {'blogs': blog})
